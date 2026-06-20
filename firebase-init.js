85685ada9da1b301a02ecac1dc3f0cff76805033
Aron John <204282932+Aronjohn1@users.noreply.github.com>
2026-06-21 02:00:55 +0800

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkdzN9WSpfccnMI_2_9JoAOI8xp8_tpw8",
-  authDomain: "web-based-8afc9.firebaseapp.com",
-  projectId: "web-based-8afc9",
-  storageBucket: "web-based-8afc9.firebasestorage.app",
-  messagingSenderId: "674288389573",
-  appId: "1:674288389573:web:c4f3e41b569088801b0fac",
-  measurementId: "G-RPG4DLFG13"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
